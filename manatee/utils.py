b95e152715732b27a4d5888a16096d2e7c7a7552
Quentin CAUDRON <quentincaudron@gmail.com>
2016-05-28 16:01:17 -0700

    Have utils.silence() only silence one context at a time.

--- manatee/utils.py
+++ manatee/utils.py
@@ -1,27 +1,11 @@
-from pyspark.sql.context import SQLContext, HiveContext
-from pyspark.context import SparkContext
-
-def silence(context=None):
+def silence(context):
     """
     Silences the PySpark log messages.
 
     Removes INFO and WARN messages from the specified context, leaving only ERROR messages.
-    If no context is passed, this function looks through globals() to find any SparkContexts,
-    SQLContexts, and HiveContexts, and silences those.
     """
 
-    if context is not None:
-        logger = context._jvm.org.apache.log4j
-        logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
-        logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
-        logger.LogManager.getRootLogger().setLevel(logger.Level.ERROR)
-        print("Silenced {}.".format(context))
-
-    else:
-        to_be_silenced = [var for var in globals().values()
-                          if (isinstance(var, HiveContext)
-                           or isinstance(var, SQLContext)
-                           or isinstance(var, SparkContext))]
-
-        for context in to_be_silenced:
-            silence(context)
+    logger = context._jvm.org.apache.log4j
+    logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
+    logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
+    logger.LogManager.getRootLogger().setLevel(logger.Level.ERROR)
