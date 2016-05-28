6f3446988249eb784b5a37430366c4dcdc5ce339
Quentin CAUDRON <quentincaudron@gmail.com>
2016-05-28 16:04:47 -0700

    Building docs.

--- manatee/utils.py
+++ manatee/utils.py
@@ -3,6 +3,12 @@ def silence(context):
     Silences the PySpark log messages.
 
     Removes INFO and WARN messages from the specified context, leaving only ERROR messages.
+
+    Parameters
+    ----------
+    context : SparkContext, SQLContext, or HiveContext
+        For example, if `sc` is your SparkContext, calling `silence(sc)` will switch
+        off INFO and WARN messages.
     """
 
     logger = context._jvm.org.apache.log4j
